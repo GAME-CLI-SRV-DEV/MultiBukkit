98a93f78ec9dbfdd7f7a6a705dead0cd8c048234
Outfit8TSB <sunnybunnies09@naver.com>
2024-07-16 18:10:18 +0900

    Update Main.java

--- src/main/java/org/MultiNet/Multibukkit/Main.java
+++ src/main/java/org/MultiNet/Multibukkit/Main.java
@@ -1,19 +1,6 @@
 package org.MultiNet.Multibukkit;
 
 import org.MultiNet.Multibukkit.start;
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.Player;
-import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
-import net.minestom.server.instance.*;
-import net.minestom.server.instance.batch.ChunkBatch;
-import net.minestom.server.instance.block.Block;
-import net.minestom.server.coordinate.Pos;
-import net.minestom.server.world.biomes.Biome;
-import java.util.Arrays;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 public class Main {
 
 
