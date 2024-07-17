01d00f3e816d7901b50f159f59c0fe983a354d42
Outfit8TSB <sunnybunnies09@naver.com>
2024-07-17 22:58:55 +0900

    Update start.java

--- src/main/java/org/MultiNet/Multibukkit/start.java
+++ src/main/java/org/MultiNet/Multibukkit/start.java
@@ -10,17 +10,28 @@ import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.Block;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginLoadOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.io.file
+    
 public class start {
+    private static final MinestomServer server = new MinestomServer();
+
     public static final Logger logger = LoggerFactory.getLogger(Main.class);
     public void what() {
         System.out.println("loading MultiBukkit. Please Wait...");
         logger.info("loading...");
+        
         config config = new config();
-        logger.info("");
+        System.out.println("Loading Bukkit Plugins...")
+            Bukkit.setServer(server);
+            server.loadPlugins();
         // Initialization
+        System.out.println(Enabling Bukkit.);
+        server.enablePlugins(PluginLoadOrder.STARTUP);
         MinecraftServer minecraftServer = MinecraftServer.init();
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         // Create the instance
@@ -36,13 +47,8 @@ public class start {
             player.setRespawnPoint(new Pos(0, 42, 0));
         });
 
-        if (!config.dualstack) {
+        
         minecraftServer.start( "0.0.0.0", config.port);
-
-    } else {
-            minecraftServer.start( "0.0.0.0", config.port);
-            minecraftServer.start( "[::]", config.ipv6port);
-        }
-   }
+        server.enablePlugins(PluginLoadOrder.POSTWORLD);
+    }
 }
-
