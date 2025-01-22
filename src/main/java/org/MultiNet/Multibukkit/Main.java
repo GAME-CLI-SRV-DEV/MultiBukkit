9e279edbc11a1f090a4c2f41779b0150b02c68bf
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-23 01:08:48 +0900

    Update Main.java

--- src/main/java/org/MultiNet/Multibukkit/Main.java
+++ src/main/java/org/MultiNet/Multibukkit/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        System.out.println("Starting Minestom Server... Please Wait...");
         start start = new start();
         start.what();
 
